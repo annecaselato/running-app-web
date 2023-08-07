302c463887e423e50be134b188755e9e78cef46d
Anne Caselato <annecaselato@gmail.com>
2023-08-07 20:03:24 -0300

    feat: add sign-up and sign-in templates

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SignIn } from "./pages/SignIn";
 import { Home } from "./pages/Home";
+import { SignUp } from "./pages/SignUp";
 
 export const AppRoutes = () => {
   return(
-    <BrowserRouter>
-      <Routes>
-          <Route path="/home" element={<Home />} />
-      </Routes>
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
     </BrowserRouter>
   )
 };
