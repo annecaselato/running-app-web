2ff4e4432f918bc09d8ee3b8b63dc9f6577e8fc7
Anne Caselato <annecaselato@gmail.com>
2023-08-14 17:26:15 -0300

    feat: customize MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import { AppRoutes } from './AppRoutes';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import { theme } from './theme';
 
 function App() {
   return (
-    <AppRoutes />
+    <ThemeProvider theme={theme}>
+      <AppRoutes />
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#0a9094'
+    },
+    secondary: {
+      main: '#f7a16c'
+    }
+  }
+});
