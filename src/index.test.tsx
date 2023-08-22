55201d42b6886bafce3e3016e24698d248347174
Anne Caselato <annecaselato@gmail.com>
2023-08-21 21:33:13 -0300

    chore: setup communication with the API

---
+++ src/index.test.tsx
@@ -0,0 +1,20 @@
+import { render, waitFor } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import App from './App';
+
+jest.mock('./App', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div className="App">Mocked App Component</div>)
+}));
+
+test('renders the app', () => {
+  const { container } = render(
+    <MockedProvider>
+      <App />
+    </MockedProvider>
+  );
+
+  waitFor(() => {
+    expect(container.getElementsByClassName('App')).toBeInTheDocument();
+  });
+});
